927bd92bca47c1a92a2b12fc38b760cac4245264
KOMPUTER\Klakson <sieczkowski.l@gmail.com>
2018-11-11 12:49:17 +0100

    Mediator first example

---
+++ src/main/java/designPatterns/behavioral/mediator/first/ChatRoom.java
@@ -0,0 +1,9 @@
+package designPatterns.behavioral.mediator.first;
+
+import java.util.Date;
+
+public class ChatRoom {
+    public static void showMessage(User user, String message){
+        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
+    }
+}
