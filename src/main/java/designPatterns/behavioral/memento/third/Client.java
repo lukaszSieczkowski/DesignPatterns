dddcad2b76736da847c0c79762e9a0ebd17af049
KOMPUTER\Klakson <sieczkowski.l@gmail.com>
2018-11-11 12:16:56 +0100

    Memento third example

---
+++ src/main/java/designPatterns/behavioral/memento/third/Client.java
@@ -0,0 +1,19 @@
+package designPatterns.behavioral.memento.third;
+
+public class Client {
+    public static void main(String[] args) {
+
+        FileWriterCaretaker caretaker = new FileWriterCaretaker();
+
+        FileWriterUtil fileWriter = new FileWriterUtil("data.txt");
+        fileWriter.write("First Set of Data\n");
+        System.out.println(fileWriter+"\n\n");
+
+        caretaker.save(fileWriter);
+        fileWriter.write("Second Set of Data\n");
+        System.out.println(fileWriter+"\n\n");
+
+        caretaker.undo(fileWriter);
+        System.out.println(fileWriter+"\n\n");
+    }
+}
---
+++ src/main/java/designPatterns/behavioral/memento/third/FileWriterCaretaker.java
@@ -0,0 +1,14 @@
+package designPatterns.behavioral.memento.third;
+
+public class FileWriterCaretaker {
+
+    private Object obj;
+
+    public void save(FileWriterUtil fileWriter){
+        this.obj=fileWriter.save();
+    }
+
+    public void undo(FileWriterUtil fileWriter){
+        fileWriter.undoToLastSave(obj);
+    }
+}
